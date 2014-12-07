3602a80ca3204df670fc8f3498cdcf434c8a1991
Michal Vanko <mklakakilli@gmail.com>
2014-12-07 17:28:04 +0100

    Nodemon setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ var gulp = require('gulp');
 // Plugins
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var nodemon = require('gulp-nodemon');
+
 
 var SOURCE = {
-  CLIENT : './client/**/!(*.spec).js',
+  CLIENT : './client/**/!(*.spec|*.mock).js',
   SERVER : './server/**/!(*.spec).js',
+
+  APP : './server/app.js',
 
   CLIENT_SPEC : './client/**/*.spec.js',
   SERVER_SPEC : './server/**/*spec.js'
@@ -17,4 +21,10 @@ gulp.task('lint', function() {
   return gulp.src([SOURCE.CLIENT, SOURCE.SERVER])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+
+/** Main development task */
+gulp.task('serve', ['lint'], function() {
+  nodemon({ script: SOURCE.APP, env: { 'NODE_ENV': 'development' } , watch: ['server/*'], ext: 'js'});
 });
