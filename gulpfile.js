f6378f02637e339960edb3e674d20c40cc38bf9c
Michal Vanko <mklakakilli@gmail.com>
2014-12-07 19:28:02 +0100

    Livereload setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var nodemon = require('gulp-nodemon');
-
+var livereload = require('gulp-livereload');
 
 var SOURCE = {
   CLIENT : './client/**/!(*.spec|*.mock).js',
   SERVER : './server/**/!(*.spec).js',
+  STATIC : './client/**/*(*.html|*.css)',
 
   APP : './server/app.js',
 
@@ -23,8 +24,21 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch([SOURCE.CLIENT, SOURCE.STATIC], ['reload']);
+});
+
+gulp.task('reload', function() {
+  livereload.changed();
+});
 
 /** Main development task */
-gulp.task('serve', ['lint'], function() {
-  nodemon({ script: SOURCE.APP, env: { 'NODE_ENV': 'development' } , watch: ['server/*'], ext: 'js'});
+gulp.task('serve', ['lint', 'watch'], function() {
+  nodemon({ script: SOURCE.APP, env: { 'NODE_ENV': 'development' } , watch: ['server/*'], ext: 'js'})
+    .on('start', function() {
+      setTimeout(function() {
+        livereload.changed();
+      }, 2000);
+    });
 });
