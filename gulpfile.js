477c78aabbe20a3331934882b41265873f6ec477
Michal Vanko <mklakakilli@gmail.com>
2014-12-14 02:23:53 +0100

    Injecting CSS and JS and order of files

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,16 @@ var concat = require('gulp-concat');
 var path = require('path');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
+var inject = require('gulp-inject');
+var order = require('gulp-order');
+
 
 var SOURCE = {
   CLIENT : {
     js: './client/**/!(*.spec|*.mock).js',
-    less: './client/**/*.less'
+    less: './client/**/*.less',
+    css: './client/**/*.css',
+    indexHtml: './client/index.html'
   },
   SERVER : './server/**/!(*.spec).js',
   STATIC : './client/**/*(*.html|*.css)',
@@ -33,12 +38,20 @@ gulp.task('lint', function() {
 });
 
 gulp.task('watch', function() {
+
+  gulp.watch([SOURCE.CLIENT.js, SOURCE.STATIC], ['reload'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', reloading...');
+    });
+
+  // Watch less stylesheets and compile them
+  gulp.watch(SOURCE.CLIENT.less, ['less']);
+
   livereload.listen();
-  gulp.watch([SOURCE.CLIENT.js, SOURCE.STATIC], ['reload']);
 });
 
 gulp.task('reload', function() {
-  livereload.changed();
+  livereload.changed('Client');
 });
 
 gulp.task('less', function() {
@@ -48,16 +61,29 @@ gulp.task('less', function() {
       path.dirname = '';
     }))
     .pipe(gulp.dest('./client/generated/css'));
-})
+});
 
 //TODO INJECT CSS JS
+gulp.task('inject', function() {
+  var sources = gulp.src([SOURCE.CLIENT.css, SOURCE.CLIENT.js], {read: false})
+   .pipe(order([
+     'client/**/reset.css',
+     'client/**/main.css',
+     'client/**/*.css'
+     ], {base: '.'}));
+     // TODO: Add JS order
+
+  return gulp.src(SOURCE.CLIENT.indexHtml)
+    .pipe(inject(sources, {relative : true}))
+    .pipe(gulp.dest('./client'));
+});
 
 /** Main development task */
-gulp.task('serve', ['lint', 'watch'], function() {
+gulp.task('serve', ['lint', 'less', 'inject', 'watch'], function() {
   nodemon({ script: SOURCE.APP, env: { 'NODE_ENV': 'development' } , watch: ['server/*'], ext: 'js'})
     .on('start', function() {
       setTimeout(function() {
-        livereload.changed();
+        livereload.changed('Server');
       }, 2000);
     });
 });
