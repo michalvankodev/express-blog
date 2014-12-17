676f0a166069b6cfe501aa5b9913f08cb0585ff5
MichalVanko <michal.vanko@gigup.eu>
2014-12-17 16:29:53 +0100

    added ui.router as dependency

---
+++ admin/static/js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+/**
+* app.js
+*
+* Settings for the angular app.
+* In this file you can setup routes and other configuration for the angular app
+*/
+var app = angular.module('blogCms',['ui.router']);
