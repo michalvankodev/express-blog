446188d723e45832774a3c7e9986658bf293b156
MichalVanko <michal.vanko@gigup.eu>
2014-12-17 15:44:43 +0100

    Setup of admin static files into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,14 @@ var SOURCE = {
     css: './client/**/*.css',
     indexHtml: './client/index.html'
   },
+  ADMIN : {
+    js: './admin/**/!(*.spec|*.mock).js',
+    less: './admin/**/*.less',
+    css: './admin/**/*.css',
+    indexHtml: './admin/index.html'
+  },
   SERVER : './server/**/!(*.spec).js',
-  STATIC : './client/**/*(*.html|*.css)',
+  STATIC : './**/*(*.html|*.css)',
 
   APP : './server/app.js',
 
@@ -41,7 +47,7 @@ gulp.task('lint', function() {
 
 gulp.task('watch', function() {
 
-  gulp.watch([SOURCE.CLIENT.js, SOURCE.STATIC], ['reload'])
+  gulp.watch([SOURCE.CLIENT.js, SOURCE.ADMIN.js, SOURCE.STATIC], ['reload'])
     .on('change', function(event) {
       gutil.log('File ' + event.path + ' was ' + event.type + ', reloading...');
     });
@@ -58,10 +64,10 @@ gulp.task('reload', function() {
 
 // Delete previously generated CSS files
 gulp.task('clean-css', function(cb) {
-  del('./client/generated/css', cb);
+  del('./**/generated/css', cb);
 });
 
-gulp.task('less', function() {
+gulp.task('less-client', function() {
   return gulp.src(SOURCE.CLIENT.less)
     .pipe(less())
     .pipe(rename(function(path) {
@@ -70,21 +76,45 @@ gulp.task('less', function() {
     .pipe(gulp.dest('./client/generated/css'));
 });
 
+gulp.task('less-admin', function() {
+  return gulp.src(SOURCE.ADMIN.less)
+  .pipe(less())
+  .pipe(rename(function(path) {
+    path.dirname = '';
+  }))
+  .pipe(gulp.dest('./admin/generated/css'));
+});
 
-//TODO INJECT CSS JS
-gulp.task('inject', ['less'], function() {
+
+gulp.task('inject-client', ['less-client'], function() {
   var sources = gulp.src([SOURCE.CLIENT.css, SOURCE.CLIENT.js], {read: false})
    .pipe(order([
      'client/**/reset.css',
      'client/**/main.css',
      'client/**/*.css'
-     ], {base: '.'}));
+   ], {base: '.'}));
      // TODO: Add JS order
 
   return gulp.src(SOURCE.CLIENT.indexHtml)
     .pipe(inject(sources, {relative : true}))
     .pipe(gulp.dest('./client'));
 });
+
+gulp.task('inject-admin', ['less-admin'], function() {
+  var sources = gulp.src([SOURCE.ADMIN.css, SOURCE.ADMIN.js], {read: false})
+    .pipe(order([
+      'admin/**/main.css',
+      'admin/**/*.css',
+      'admin/**/app.js',
+      'admin/**/*.js'
+    ], {base: '.'}));
+
+  return gulp.src(SOURCE.ADMIN.indexHtml)
+    .pipe(inject(sources, {relative : true}))
+    .pipe(gulp.dest('./admin'));
+});
+
+gulp.task('inject', ['inject-client', 'inject-admin']);
 
 /** Main development task */
 gulp.task('serve', ['lint', 'inject'], function() {
