8e70454fb41529788a939765cbbad63e2ce26955
Michal Vanko <mklakakilli@gmail.com>
2015-01-14 17:37:51 +0100

    Added karma config init

---
+++ karma.conf.js
@@ -0,0 +1,12 @@
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+
+    browsers: [Firefox, Chrome]
+
+  });
+};
