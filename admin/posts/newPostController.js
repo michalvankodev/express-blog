1049624e16ef1e1ed40cc13497ab828587a2fcbb
Michal Vanko <mklakakilli@gmail.com>
2015-01-26 16:44:57 +0100

    New Post adding succesfully

--- admin/posts/newPostController.js
+++ admin/posts/newPostController.js
@@ -2,6 +2,19 @@
 
 var app = angular.module('blogCms');
 
-app.controller('NewPostController', function($scope) {
+app.controller('NewPostController', function($scope, $http) {
+  $scope.saving = false;
+  $scope.post = {};
 
+  $scope.savePost = function savePost() {
+    $scope.saving = true;
+    $http.post('/api/posts/', $scope.post)
+      .success(function(data){
+        $scope.saving = false;
+        $scope.post.id = data._id;
+      })
+      .error(function(data){
+        console.log(data);
+      });
+  }
 });
